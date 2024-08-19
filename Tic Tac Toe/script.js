eb806d26eb0ea17fe5e9e05dd049e857060608a2
hawkgenesys <106819343+hawkgenesys@users.noreply.github.com>
2024-08-19 17:51:29 +0530

    Add files via upload

---
+++ Tic Tac Toe/script.js
@@ -0,0 +1,89 @@
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameActive = true;
+
+const statusDisplay = document.getElementById('status');
+const boardElement = document.getElementById('board');
+const resetButton = document.getElementById('resetButton');
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const updateStatus = (message) => {
+    statusDisplay.textContent = message;
+}
+
+const handleCellPlayed = (clickedCell, clickedCellIndex) => {
+    board[clickedCellIndex] = currentPlayer;
+    clickedCell.textContent = currentPlayer;
+}
+
+const handlePlayerChange = () => {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    updateStatus(`It's ${currentPlayer}'s turn`);
+}
+
+const handleResultValidation = () => {
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        let a = board[winCondition[0]];
+        let b = board[winCondition[1]];
+        let c = board[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        updateStatus(`Player ${currentPlayer} has won!`);
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !board.includes("");
+    if (roundDraw) {
+        updateStatus("Game ended in a draw!");
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+const handleCellClick = (clickedCellEvent) => {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
+
+    if (board[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+const handleRestartGame = () => {
+    gameActive = true;
+    currentPlayer = "X";
+    board = ["", "", "", "", "", "", "", "", ""];
+    updateStatus(`It's ${currentPlayer}'s turn`);
+    document.querySelectorAll('.cell').forEach(cell => cell.textContent = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+resetButton.addEventListener('click', handleRestartGame);
+
+updateStatus(`It's ${currentPlayer}'s turn`);
